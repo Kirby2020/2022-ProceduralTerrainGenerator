1cb96572cc242204ca2210cc64d819f978c7efb4
Kirby2020 <74135383+Kirby2020@users.noreply.github.com>
2022-11-28 12:07:04 +0100

    Add debug screen

---
+++ Procedural Terrain/Assets/Scripts/GuiController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GuiController : MonoBehaviour {
+    [SerializeField] private CameraController playerCamera;
+    [SerializeField] private Text debugText;
+
+    // Update is called once per frame
+    void Update() {
+        var postion = playerCamera.transform.position;
+        float x = Mathf.Round(postion.x);
+        float y = Mathf.Round(postion.y);
+        float z = Mathf.Round(postion.z);
+        int chunkX = Mathf.FloorToInt(x / 16);
+        int chunkZ = Mathf.FloorToInt(z / 16);
+        debugText.text = $"X: {x}\tY: {y}\tZ: {z}\n";
+        debugText.text += $"Chunk: {chunkX}, {chunkZ}\n";
+    }
+}
