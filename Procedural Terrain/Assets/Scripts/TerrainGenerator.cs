a7f4b54b28b8f05257b342e583e98e0606d8d1d2
Kirby2020 <74135383+Kirby2020@users.noreply.github.com>
2022-11-28 12:08:22 +0100

    Generate terrain with noise

--- Procedural Terrain/Assets/Scripts/TerrainGenerator.cs
+++ Procedural Terrain/Assets/Scripts/TerrainGenerator.cs
@@ -1,41 +1,70 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TerrainGenerator : MonoBehaviour {
+    [SerializeField] private Transform player;
     private FractalNoise terrainNoise; // Main noise map for terrain height
-    private const int MAX_HEIGHT = 20;
+    private const int MAX_HEIGHT = 40;
     private const int MIN_HEIGHT = 0;
+    private const int CHUNK_SIZE = 16;
+    private int renderDistance = 8;
+    private int seaLevel = 40;
 
-    private int seaLevel = 10;
-    private int maxX = 50;
-    private int maxZ = 50;
 
     private void Awake() {
-        terrainNoise = ScriptableObject.CreateInstance<FractalNoise>();
+        SetTerrainNoise();
         GenerateTerrain();
     }
 
+    private void Update() {
+        if (player.position.x > CHUNK_SIZE || player.position.z > CHUNK_SIZE) {
+            GenerateTerrain();
+        }
+
+    }
+
+    private void SetTerrainNoise() {
+        terrainNoise = ScriptableObject.CreateInstance<FractalNoise>();
+        terrainNoise.SetSeed(0);
+        terrainNoise.Amplitude = MAX_HEIGHT;
+        terrainNoise.Frequency = 0.005f;
+        terrainNoise.Octaves = 4;
+        terrainNoise.Lacunarity = 2f;
+        terrainNoise.Persistence = 0.5f;        
+    }
+
     private void GenerateTerrain() {
-        for (int x = 0; x < maxX; x++) {
-            for (int z = 0; z < maxZ; z++) {
-                //int y = Mathf.FloorToInt((float)terrainNoise.Noise(x, z) * MAX_HEIGHT);
-                for (int y = 0; y < MAX_HEIGHT; y++) {
-                    PlaceBlock(x, y, z);
-                }
+        int minX = (int)player.position.x;
+        int minZ = (int)player.position.z;
+        for (int x = minX; x < (int)player.position.x + CHUNK_SIZE + (int)player.position.x; x++) {
+            for (int z = minZ; z < (int)player.position.z + CHUNK_SIZE + (int)player.position.z; z++) {
+                int y = seaLevel + Mathf.FloorToInt((float)terrainNoise.NoiseCombinedOctaves(x,z) * (float)terrainNoise.Amplitude);
+                // if (y < MIN_HEIGHT || y > MAX_HEIGHT) break;
+                PlaceBlock(x, y, z);
+                //FillUnderground(x, y, z);
             }
         }
     }
 
     private void PlaceBlock(int x, int y, int z) {
-        // Dont place blocks under min height or above max height
-        if (y < MIN_HEIGHT || y > MAX_HEIGHT) return;
         GameObject block;
-
         block = Resources.Load("Blocks/StoneBlock") as GameObject;
 
         var placedBlock = Instantiate(block, new Vector3(x, y, z), Quaternion.identity);
         placedBlock.name = $"Block ({x},\t{y},\t{z})\t";
         placedBlock.transform.parent = transform;
+    }
+
+    private void FillUnderground(int x, int y, int z) {
+        for (int i = MIN_HEIGHT; i < y; i++) {
+            GameObject block;
+            block = Resources.Load("Blocks/StoneBlock") as GameObject;
+
+            var placedBlock = Instantiate(block, new Vector3(x, i, z), Quaternion.identity);
+            placedBlock.name = $"Block ({x},\t{i},\t{z})\t";
+            placedBlock.transform.parent = transform;
+        }
     }
 }
